8e33c02d10698549189e9d05ca426a24e711f2e8
Akshay <akshaykhatri22@gmail.com>
2024-08-03 23:17:50 +0530

    updated start

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PersonPage from "./components/PersonPage";
+import HomePage from "./components/HomePage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/person/:id" element={<PersonPage />} />
+        <Route path="/" element={<HomePage />} />
         {/* You can add more routes here */}
       </Routes>
     </Router>
